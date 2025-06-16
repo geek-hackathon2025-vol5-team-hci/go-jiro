1d0d678d2595349f0b3647b205df5d8fb969e33e
Tako50 <kota22650@gmail.com>
2025-06-16 12:39:55 +0900

    変更

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,4 +1,6 @@
 export default function MapPage() {
+ 
+  
   return (
     <main style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
       <h1 className="text-center mt-10 text-xl font-bold">地図ページ（仮）</h1>
