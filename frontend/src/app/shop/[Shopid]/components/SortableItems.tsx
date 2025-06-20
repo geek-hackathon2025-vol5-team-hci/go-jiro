65e61b8a0b0aaa6e7883606bf5811d4cb16068cd
Tako50 <kota22650@gmail.com>
2025-06-20 17:20:15 +0900

    変更成功

--- frontend/src/app/shop/[Shopid]/components/SortableItems.tsx
+++ frontend/src/app/shop/[Shopid]/components/SortableItems.tsx
@@ -1,4 +1,4 @@
-// components/SortableItems.tsx
+// frontend/src/app/shop/[Shopid]/components/SortableItems.tsx
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import React from "react";
