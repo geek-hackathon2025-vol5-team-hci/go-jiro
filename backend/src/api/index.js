844b445fc0bd17d2e8ce90dc9b53d2225ffeaaed
spiral987 <41581839+spiral987@users.noreply.github.com>
2025-06-22 09:29:33 +0900

    mji

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -4,21 +4,15 @@ const authRouter = require('./auth');
 const messageRouter = require('./messages');
 const shopsRouter = require('./shops');
 const logRouter = require('./logs');
-<<<<<<< HEAD
 const usersRouter = require('./users'); // 追記
-=======
 const evaluationRouter = require('./evaluations');
->>>>>>> develop
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/messages', messageRouter); // '/api/message' へのリクエストをmessageRouterに流す
 router.use('/logs', logRouter); // '/api/logs' へのリクエストをlogRouterに流す
 router.use('/shops', shopsRouter); // '/api/shopへのリクエストをshopsRouterに流す
-<<<<<<< HEAD
 router.use('/users', usersRouter); // 追記: '/api/users'へのリクエストをusersRouterに流す
-=======
 router.use('/evaluations', evaluationRouter); 
->>>>>>> develop
 
 module.exports = router;
