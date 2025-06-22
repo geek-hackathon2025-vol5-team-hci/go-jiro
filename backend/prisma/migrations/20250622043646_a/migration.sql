37610f04abd9a2e429bd2c3c78eb1c9cb31a095f
spiral987 <41581839+spiral987@users.noreply.github.com>
2025-06-22 13:37:07 +0900

    migration

--- backend/prisma/migrations/20250622043646_a/migration.sql
+++ backend/prisma/migrations/20250622043646_a/migration.sql
@@ -51,6 +51,32 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Evaluation" (
+    "id" SERIAL NOT NULL,
+    "shopId" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "jirodo" DOUBLE PRECISION NOT NULL,
+    "estimatePortion" INTEGER NOT NULL,
+    "actualPortion" INTEGER NOT NULL,
+    "orderHelp" INTEGER NOT NULL,
+    "exitPressure" INTEGER NOT NULL,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Evaluation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "session" (
+    "sid" TEXT NOT NULL,
+    "sess" JSONB NOT NULL,
+    "expire" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("sid")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "CallRule_shopId_category_option_key" ON "CallRule"("shopId", "category", "option");
 
@@ -63,5 +89,14 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
+-- CreateIndex
+CREATE INDEX "session_expire_idx" ON "session"("expire");
+
 -- AddForeignKey
 ALTER TABLE "CallRule" ADD CONSTRAINT "CallRule_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Evaluation" ADD CONSTRAINT "Evaluation_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Evaluation" ADD CONSTRAINT "Evaluation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
